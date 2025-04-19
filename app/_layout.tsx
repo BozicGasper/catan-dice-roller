3b06fc3174759e9cc9bfe857cd4d5635f605c242
Gašper Božič <gasper.bozic@smartis.si>
2025-04-19 09:37:39 +0200

    [main] additional changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { useThemeStore } from '@/store/themeStore';
+import { Slot } from 'expo-router';
 
 export default function RootLayout() {
   useFrameworkReady();
@@ -11,10 +12,7 @@ export default function RootLayout() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" options={{ presentation: 'modal' }} />
-      </Stack>
+      <Slot />
       <StatusBar style={isDark ? 'light' : 'dark'} />
     </View>
   );
